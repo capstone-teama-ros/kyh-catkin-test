a6f8b63404d2b738fb5066d6c5c76ff1ddbde54e
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-05 19:51:32 +0900

    feat: Show found lines

--- src/wbl/src/wbl_node.cpp
+++ src/wbl/src/wbl_node.cpp
@@ -2,6 +2,9 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 #include "line_finder.h"
 
@@ -10,23 +13,68 @@ class LidarListener
 public:
   LidarListener()
   {
-    line_finder_.setResolution(0.015);
-    line_finder_.setThreshold(20);
-    line_finder_.setTheta(1 * M_PI / 180);
+    line_finder_.setThreshold(15);
+    line_finder_.setRho(0.015);
+    line_finder_.setTheta(2 * M_PI / 180);
+    line_finder_.setBufferSize(500);
   }
 
   void callback(const sensor_msgs::LaserScan::ConstPtr& message)
   {
     line_finder_.findLines(lines_, message);
-    for (size_t i = 0; i < lines_.size(); ++i)
+
+    image_ = cv::Mat::zeros(image_size_, image_size_, CV_8UC3);
+
+    // Draw LIDAR points
+    for (size_t i = 0; i < message->ranges.size(); ++i)
     {
-      ROS_INFO("Line %lu: rho = %f [m], theta = %f [rad]", i, lines_[i][0], lines_[i][1]);
+      float range = message->ranges[i];
+      if (range < message->range_min || range > message->range_max)
+        continue;
+
+      float angle = message->angle_min + message->angle_increment * i;
+
+      // Transform polar coordinates (origin at robot) to cartesian (origin at left top of image)
+      float point_x = range * std::cos(angle);
+      float point_y = range * std::sin(angle);
+      int cx = cvRound(image_.cols / 2.0 + point_x * pixels_per_meter);
+      int cy = cvRound(image_.rows / 2.0 + point_y * pixels_per_meter);
+
+      const int DOT_SIZE = 2;
+      cv::Rect pointRect(cx - DOT_SIZE, cy - DOT_SIZE, 2 * DOT_SIZE, 2 * DOT_SIZE);
+      cv::rectangle(image_, pointRect, cv::Scalar(255, 255, 0), CV_FILLED);
     }
+
+    // Draw lines found
+    for (auto& line : lines_)
+    {
+      auto rho = line[0];
+      auto theta = line[1];
+      auto cos_theta = std::cos(theta);
+      auto sin_theta = std::sin(theta);
+
+      auto cx = rho * cos_theta * pixels_per_meter + image_.cols / 2.0;
+      auto cy = rho * sin_theta * pixels_per_meter + image_.rows / 2.0;
+
+      // Create two distant points on the line
+      int ax = cvRound(cx + 100 * image_.cols * sin_theta);
+      int ay = cvRound(cy - 100 * image_.rows * cos_theta);
+      int bx = cvRound(cx - 100 * image_.cols * sin_theta);
+      int by = cvRound(cy + 100 * image_.rows * cos_theta);
+
+      cv::line(image_, { ax, ay }, { bx, by }, cv::Scalar(0, 255, 255));
+    }
+
+    cv::imshow("Wall Detection", image_);
+    cv::waitKey(100);
   }
 
 private:
   LineFinder line_finder_;
   std::vector<cv::Vec2f> lines_;
+  int image_size_ = 600;
+  cv::Mat image_;
+  double pixels_per_meter = 80;
 };
 
 int main(int argc, char** argv)
