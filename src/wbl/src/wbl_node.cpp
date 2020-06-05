8f02a329d7c3393b5f06b984df00f5221c92306d
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-05 13:47:27 +0900

    feat: Add empty LaserScan subscriber to /scan
    Add an empty subscriber to "/scan" topic with message queue size == 1000
    Use class method callback instead of plain function callback.
    This allows me to reuse variables between callback calls.

--- src/wbl/src/wbl_node.cpp
+++ src/wbl/src/wbl_node.cpp
@@ -1,9 +1,21 @@
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+class LidarListener
+{
+public:
+  void callback(const sensor_msgs::LaserScan::ConstPtr& message)
+  {
+  }
+};
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "wbl");
   ros::NodeHandle n;
+
+  LidarListener lidar_listener;
+  ros::Subscriber lidar_subscriber = n.subscribe("/scan", 1000, &LidarListener::callback, &lidar_listener);
 
   ros::Rate sleep_duration(50);  // milliseconds
 
