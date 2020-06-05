f5b1b2052167fca7884199b70e17882ebdd99100
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-05 17:21:57 +0900

    feat: Always run callbacks whenever there is a message

--- src/wbl/src/wbl_node.cpp
+++ src/wbl/src/wbl_node.cpp
@@ -37,11 +37,5 @@ int main(int argc, char** argv)
   LidarListener lidar_listener;
   ros::Subscriber lidar_subscriber = n.subscribe("/scan", 1000, &LidarListener::callback, &lidar_listener);
 
-  ros::Rate sleep_duration(50);  // milliseconds
-
-  while (ros::ok())
-  {
-    ros::spinOnce();
-    sleep_duration.sleep();
-  }
+  ros::spin();
 }
